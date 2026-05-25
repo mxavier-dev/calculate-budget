2a54e472a270d7f0da59e819eedd551a927a380f
Matheus Xavier <matheus.fx456@gmail.com>
2026-05-25 18:58:21 -0300

    fix: adjust budget calculation for months beyond five

--- services/csv_service.py
+++ services/csv_service.py
@@ -4,7 +4,7 @@ from datetime import datetime
 today = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
 
 def gerar_csv(parcelas, realstate):
-    datas = [{'Month': i, 'Value': f'{parcelas / 12:.2f}'} for i in range(1,13)]
+    datas = [{'Month': i, 'Value': f'{(parcelas / 12 if i > 5 else (parcelas / 12) + 400):.2f}'} for i in range(1,13)]
     with open(f'data/Budget_{realstate}_{today}.csv', 'a', encoding='utf-8') as arq:
             writer = csv.DictWriter(arq, fieldnames=datas[0].keys())
             writer.writeheader()
